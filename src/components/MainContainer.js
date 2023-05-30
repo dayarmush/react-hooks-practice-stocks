1a8321cd0a506c1ad63a555939b7f066f5ee0a67
dovid yarmush <yarmushda@gmail.com>
2023-05-30 11:45:09 -0400

    render stocks on page

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+  const [stocks, setStocks] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+    .then(resp => resp.json())
+    .then(data => setStocks(data))
+  }, [])
+
+  if (!stocks) return <h3>Loading...</h3>
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
